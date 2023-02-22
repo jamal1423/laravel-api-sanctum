b027e46701ea04ca08c5c89a61156e349446e335
Mochammad Jamal <64123061+jamal1423@users.noreply.github.com>
2023-02-22 08:21:00 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,28 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//API route for register new user
 Route::post('/register', [AuthController::class, 'register']);
-//API route for login user
 Route::post('/login', [AuthController::class, 'login']);
 
-//Protecting Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    // Route::get('/profile', function(Request $request) {
-    //     // return auth()->user();
-    //     return response()->json([
-    //         'token' => $request->bearerToken()
-    //     ]);
-    // });
-
     Route::get('/profile', [ProfileController::class, 'data_profile']);
-
-    // Http::withHeaders([
-    //     'Accept' => 'application/json', 
-    //     'Authorization' => 'Bearer 6|JlvzZB50V8gQ8Toft2xS1pP6jRriNRKJwFL8Q8pC'
-    // ])->get('http://127.0.0.1:8000/api/profile');
-
-    // API route for logout user
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
